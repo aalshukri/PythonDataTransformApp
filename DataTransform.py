8081c238ea8ffcac9d2861db0139e9c856227d4a
Ayesh Alshukri <a.alshukri@liverpool.ac.uk>
2017-05-10 12:14:52 +0100

    App stub

---
+++ DataTransform.py
@@ -0,0 +1,22 @@
+#
+# DataTransform
+#
+# @created 2017-05-10 11:56:12 
+# @version 0.01 (beta)
+# @author 
+# @email 
+#
+# PythonDataTransformApp
+#
+#
+# How to run:
+# > python DataTransform.py
+#
+
+
+class DataTransform(object):	
+	'Data Transform App'
+	pass
+
+
+
